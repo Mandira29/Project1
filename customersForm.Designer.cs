23e04ef8707db5aace28b210703481d462a126b6
Mandira <desilvamandira6@gmail.com>
2026-06-04 21:50:32 +0530

    1.Added customersForm 2. Added the database 3.delete the signup form i created and  kept the signup form they uploaded
    Add customersForm UserControl and resource files
    
    Introduced the `customersForm` UserControl with a panel, label, and DataGridView for displaying customer data. Included code-behind, designer, and .resx files. Also added a default resource file for `signupForm`.

---
+++ customersForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Project1
+{
+    partial class customersForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(dataGridView1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(10, 18);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1130, 730);
+            panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(14, 42);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1099, 672);
+            dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 20);
+            label1.TabIndex = 0;
+            label1.Text = "All Customers";
+            // 
+            // customersForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel1);
+            Name = "customersForm";
+            Size = new Size(1157, 756);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private DataGridView dataGridView1;
+        private Label label1;
+    }
+}
---
+++ customersForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class customersForm : UserControl
+    {
+        public customersForm()
+        {
+            InitializeComponent();
+            displayCustomers();
+        }
+
+        public void displayCustomers()
+        {
+            //customersList cData = new customersList();
+            //List<customersList> listData = cData.customersListData();
+
+           // dataGridView1.DataSource = listData;
+
+        }
+    }
+}
