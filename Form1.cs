4964c42b9e3ef10d0a59c5bc07d526409d16570e
sachintha-ramesh <sachintharameshchandana@gmail.com>
2026-06-06 15:29:20 +0530

    signup form rebuild fixed erros

--- Form1.cs
+++ Form1.cs
@@ -83,5 +83,10 @@ namespace Project1
             signupForm.Show();
             this.Hide();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
