3689f663c7ab498afae171bcb3dd1ac0a02508aa
ThiliniImeshika <thiliniimeshika2003@gmail.com>
2026-06-09 12:58:05 +0530

    Added dashboard

---
+++ dashboardForm.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Project1
+{
+    partial class dashboardForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel6 = new Panel();
+            dashboard_totalRevenue = new Label();
+            label8 = new Label();
+            pictureBox4 = new PictureBox();
+            panel5 = new Panel();
+            dashboard_todaysRevenue = new Label();
+            label6 = new Label();
+            pictureBox3 = new PictureBox();
+            panel4 = new Panel();
+            dashboard_totalProducts = new Label();
+            label4 = new Label();
+            pictureBox2 = new PictureBox();
+            panel3 = new Panel();
+            dashboard_totalUsers = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            dataGridView1 = new DataGridView();
+            label9 = new Label();
+            panel1.SuspendLayout();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(panel6);
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel3);
+            panel1.Location = new Point(3, 14);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1120, 177);
+            panel1.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.FromArgb(78, 52, 46);
+            panel6.Controls.Add(dashboard_totalRevenue);
+            panel6.Controls.Add(label8);
+            panel6.Controls.Add(pictureBox4);
+            panel6.Location = new Point(854, 28);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(207, 117);
+            panel6.TabIndex = 4;
+            // 
+            // dashboard_totalRevenue
+            // 
+            dashboard_totalRevenue.AutoSize = true;
+            dashboard_totalRevenue.BackColor = Color.FromArgb(78, 52, 46);
+            dashboard_totalRevenue.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dashboard_totalRevenue.ForeColor = Color.White;
+            dashboard_totalRevenue.Location = new Point(103, 42);
+            dashboard_totalRevenue.Name = "dashboard_totalRevenue";
+            dashboard_totalRevenue.Size = new Size(18, 20);
+            dashboard_totalRevenue.TabIndex = 2;
+            dashboard_totalRevenue.Text = "0";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.FromArgb(78, 52, 46);
+            label8.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(93, 74);
+            label8.Name = "label8";
+            label8.Size = new Size(116, 20);
+            label8.TabIndex = 1;
+            label8.Text = "Total Revenue";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.money;
+            pictureBox4.Location = new Point(17, 24);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(70, 70);
+            pictureBox4.TabIndex = 0;
+            pictureBox4.TabStop = false;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(78, 52, 46);
+            panel5.Controls.Add(dashboard_todaysRevenue);
+            panel5.Controls.Add(label6);
+            panel5.Controls.Add(pictureBox3);
+            panel5.Location = new Point(600, 28);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(205, 117);
+            panel5.TabIndex = 4;
+            // 
+            // dashboard_todaysRevenue
+            // 
+            dashboard_todaysRevenue.AutoSize = true;
+            dashboard_todaysRevenue.BackColor = Color.FromArgb(78, 52, 46);
+            dashboard_todaysRevenue.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dashboard_todaysRevenue.ForeColor = Color.White;
+            dashboard_todaysRevenue.Location = new Point(103, 42);
+            dashboard_todaysRevenue.Name = "dashboard_todaysRevenue";
+            dashboard_todaysRevenue.Size = new Size(18, 20);
+            dashboard_todaysRevenue.TabIndex = 2;
+            dashboard_todaysRevenue.Text = "0";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.FromArgb(78, 52, 46);
+            label6.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(87, 74);
+            label6.Name = "label6";
+            label6.Size = new Size(118, 20);
+            label6.TabIndex = 1;
+            label6.Text = "Today revenue";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.trevenue;
+            pictureBox3.Location = new Point(17, 24);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(70, 70);
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(78, 52, 46);
+            panel4.Controls.Add(dashboard_totalProducts);
+            panel4.Controls.Add(label4);
+            panel4.Controls.Add(pictureBox2);
+            panel4.Location = new Point(353, 28);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(198, 117);
+            panel4.TabIndex = 3;
+            // 
+            // dashboard_totalProducts
+            // 
+            dashboard_totalProducts.AutoSize = true;
+            dashboard_totalProducts.BackColor = Color.FromArgb(78, 52, 46);
+            dashboard_totalProducts.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dashboard_totalProducts.ForeColor = Color.White;
+            dashboard_totalProducts.Location = new Point(103, 42);
+            dashboard_totalProducts.Name = "dashboard_totalProducts";
+            dashboard_totalProducts.Size = new Size(18, 20);
+            dashboard_totalProducts.TabIndex = 2;
+            dashboard_totalProducts.Text = "0";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.FromArgb(78, 52, 46);
+            label4.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(93, 74);
+            label4.Name = "label4";
+            label4.Size = new Size(118, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Total Products";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.openBox;
+            pictureBox2.Location = new Point(17, 24);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(70, 70);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(78, 52, 46);
+            panel3.Controls.Add(dashboard_totalUsers);
+            panel3.Controls.Add(label1);
+            panel3.Controls.Add(pictureBox1);
+            panel3.Location = new Point(55, 28);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(190, 117);
+            panel3.TabIndex = 0;
+            // 
+            // dashboard_totalUsers
+            // 
+            dashboard_totalUsers.AutoSize = true;
+            dashboard_totalUsers.BackColor = Color.FromArgb(78, 52, 46);
+            dashboard_totalUsers.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dashboard_totalUsers.ForeColor = Color.White;
+            dashboard_totalUsers.Location = new Point(103, 42);
+            dashboard_totalUsers.Name = "dashboard_totalUsers";
+            dashboard_totalUsers.Size = new Size(18, 20);
+            dashboard_totalUsers.TabIndex = 2;
+            dashboard_totalUsers.Text = "0";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(78, 52, 46);
+            label1.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(93, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Total Users";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.group;
+            pictureBox1.Location = new Point(17, 24);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(70, 70);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(label9);
+            panel2.Location = new Point(6, 210);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1117, 573);
+            panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(18, 46);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1081, 507);
+            dataGridView1.TabIndex = 1;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.ForeColor = SystemColors.GrayText;
+            label9.Location = new Point(18, 12);
+            label9.Name = "label9";
+            label9.Size = new Size(96, 20);
+            label9.TabIndex = 0;
+            label9.Text = "Today's Sales";
+            // 
+            // dashboardForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "dashboardForm";
+            Size = new Size(1157, 786);
+            panel1.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel3;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private Panel panel6;
+        private Label dashboard_totalRevenue;
+        private Label label8;
+        private PictureBox pictureBox4;
+        private Panel panel5;
+        private Label dashboard_todaysRevenue;
+        private Label label6;
+        private PictureBox pictureBox3;
+        private Panel panel4;
+        private Label dashboard_totalProducts;
+        private Label label4;
+        private PictureBox pictureBox2;
+        private Label dashboard_totalUsers;
+        private DataGridView dataGridView1;
+        private Label label9;
+    }
+}
---
+++ dashboardForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace Project1
+{
+    public partial class dashboardForm : UserControl
+    {
+        string connection = @"Data Source=.\SQLEXPRESS;Initial Catalog=resturantsystem;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        public dashboardForm()
+        {
+            InitializeComponent();
+            displayTotalProducts();
+            displayTotalUsers();
+            displayTodaysRevenue();
+            displayTotalRevenue();
+            displayTodaySales();
+        }
+
+        public void displayTodaySales()
+        {
+            customersList cData = new customersList();
+            List<customersList> todaysSales = new customersList().todaysSalescustomerListData();
+        }
+        public void displayTotalUsers()
+        {
+            using (SqlConnection connect = new SqlConnection(connection))
+            {
+                connect.Open();
+                string selectData = "SELECT COUNT(*) FROM users";
+
+                using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        int count = Convert.ToInt32(reader[0]);
+                        dashboard_totalUsers.Text = count.ToString();
+
+                    }
+                }
+
+
+            }
+        }
+
+        public void displayTotalProducts()
+        {
+            using (SqlConnection connect = new SqlConnection(connection))
+            {
+                connect.Open();
+                string selectData = "SELECT COUNT(id) FROM products";
+                using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        int count = Convert.ToInt32(reader[0]);
+                        dashboard_totalProducts.Text = count.ToString();
+                    }
+                }
+            }
+        }
+
+        public void displayTodaysRevenue()
+        {
+            using (SqlConnection connect = new SqlConnection(connection))
+            {
+                connect.Open();
+                string selectData = "SELECT SUM(CAST(total AS DECIMAL(10,2))) FROM orders where date_order = @date";
+                using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                {
+                    DateTime today = DateTime.Now;
+                    string getToday = today.ToString("yyyy-MM-dd");
+
+
+                    cmd.Parameters.AddWithValue("@date", getToday);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        if (reader[0] != DBNull.Value)
+                        {
+                            decimal count = Convert.ToDecimal(reader[0]);
+                            dashboard_todaysRevenue.Text = "$" + count.ToString("0.00");
+                        }
+                        else
+                        {
+                            dashboard_todaysRevenue.Text = "$0.00";
+                        }
+                    }
+                }
+            }
+        }
+
+        public void displayTotalRevenue()
+        {
+            using (SqlConnection connect = new SqlConnection(connection))
+            {
+                connect.Open();
+                string selectData = "SELECT SUM(CAST(total AS DECIMAL(10,2))) FROM orders ";
+                using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                {
+                  
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        if (reader[0] != DBNull.Value)
+                        {
+                            decimal count = Convert.ToDecimal(reader[0]);
+                            dashboard_totalRevenue.Text = "$" + count.ToString("0.00");
+                        }
+                        else
+                        {
+                            dashboard_totalRevenue.Text = "$0.00";
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
